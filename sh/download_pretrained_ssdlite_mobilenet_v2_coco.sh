359dbc97fdc1b4fc2ca0315d0da31b757c44c720
Kosuke Futamata <fmatasuke@gmail.com>
2018-12-06 19:28:02 +0900

    fix script to download pretrained correctly

--- sh/download_pretrained_ssdlite_mobilenet_v2_coco.sh
+++ sh/download_pretrained_ssdlite_mobilenet_v2_coco.sh
@@ -1,3 +1,3 @@
-curl -O http://download.tensorflow.org/models/object_detection/ssdlite_mobilenet_v2_coco_2018_05_09.tar.gz -o data/object_detection_api/data/pretrained/model.tar.gz
+curl http://download.tensorflow.org/models/object_detection/ssdlite_mobilenet_v2_coco_2018_05_09.tar.gz -o data/object_detection_api/data/pretrained/model.tar.gz
 tar -zxvf data/object_detection_api/data/pretrained/model.tar.gz -C data/object_detection_api/data/pretrained
 rm -rf data/object_detection_api/data/pretrained/model.tar.gz
